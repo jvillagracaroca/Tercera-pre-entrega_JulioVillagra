fb01bca87ac8a6b1267a2e8de0cc012c23c7fd6c
Julio Villagra <jvillagra@sertran.cl>
2023-05-27 15:19:48 -0400

    Agregar codigo

--- preentrega/mantenedorUsuario/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from django.contrib import admin
-
-from views import views
-
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^index/', views.index),
-]
